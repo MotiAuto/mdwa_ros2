9fee67a8788e0cbaf210486ced036b3ee25abba4
motii8128 <default270robo@gmail.com>
2024-04-22 01:12:20 +0900

    Update

---
+++ include/mdwa_ros2_node.hpp
@@ -0,0 +1,14 @@
+#ifndef MDWA_ROS2_NODE_HPP_
+#define MDWA_ROS2_NODE_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace mdwa_ros2
+{
+    class MDWANode : public rclcpp::Node
+    {
+
+    };
+}
+
+#endif
